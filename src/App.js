22152477b302ce63302e4cdf11f9d4f82e1349b8
Sebastian Martinez <sebasmartinez05@outlook.com>
2022-05-01 21:35:10 -0500

    create routes and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-me" element={<AboutMe />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <nav className="bg-white px-2 sm:px-4 py-3 dark:bg-gray-800">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <a href="/" className="flex items-center">
+            <img
+              src={require("../assets/images/yo.JPG")}
+              className="rounded-full mr-3 h-10"
+              alt="Photo of Sebastian Martínez"
+            />
+            <span className="self-center md:text-xl font-semibold dark:text-white text-clip">
+              Sebastian Martínez
+            </span>
+          </a>
+          <button
+            data-collapse-toggle="mobile-menu"
+            type="button"
+            className="inline-flex items-center mx-5 p-1 text-sm text-gray-500 rounded-lg md:hidden"
+            aria-controls="mobile-menu"
+            aria-expanded="false"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <div className="mx-auto">
+              <span
+                aria-hidden="true"
+                className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
+                  isOpen ? "rotate-45" : "-translate-y-1.5"
+                }`}
+              ></span>
+              <span
+                aria-hidden="true"
+                className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
+                  isOpen ? "opacity-0" : ""
+                }`}
+              ></span>
+              <span
+                aria-hidden="true"
+                className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
+                  isOpen ? "-rotate-45" : "translate-y-1.5"
+                }`}
+              ></span>
+            </div>
+          </button>
+          <div
+            className={`${
+              isOpen ? "" : "hidden"
+            } w-full md:block md:w-auto duration-200 transition`}
+            id="mobile-menu"
+          >
+            <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
