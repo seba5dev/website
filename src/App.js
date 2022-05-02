0482cc13d66af5f057de5a79664dc388f967fcfb
Sebastian Martinez <sebasmartinez05@outlook.com>
2022-05-01 22:43:10 -0500

    add new pages to router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
+import Projects from "./pages/Projects";
+import ContactMe from "./pages/ContactMe";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about-me" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact-me" element={<ContactMe />} />
         </Routes>
       </BrowserRouter>
     </>
