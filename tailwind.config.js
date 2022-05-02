819a9ddbe2446b328ed2c678213239d5b368504d
Sebastian Martinez <sebasmartinez05@outlook.com>
2022-05-01 22:43:18 -0500

    create new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        seba: {
+          100: "#2E0D73",
+          200: "#6446A6",
+          300: "#7289D9",
+          400: "#c38e12",
+          500: "#b04818",
+        },
+      },
+    },
   },
   plugins: [],
 };
